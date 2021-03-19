cc328d2db10b700e06805011f12bf99a55304094
Aaron Lee <amlee73@gmail.com>
2021-03-19 14:14:55 -0500

    Testing css changes

--- helper.py
+++ helper.py
@@ -21,18 +21,17 @@ skills = [["Programming Languages", [['Python', 4, '7+ years'],  # language, lev
                                      ['C++', 2, '2 years'],
                                      ['JavaScript', 2, '2 years'],
                                      ['Java', 1, '2 years'],
-                                     ['Arduino', 3, '7+ years'],
+                                     ['Arduino/Processing', 3, '7+ years'],
                                      ] ],
             ["Data Analysis", [['Data Wrangling', 4, 'Pandas, NumPy, '],  # language, level, years
-                                     ['Data Visualization', 3, 'Matplotlib, Plotly, Seaborn, Folium'],
-                                     ['Statistics', 3, 'Inferential and Descriptive'],
-                                     ['Java', 2, '2 years']
+                                     ['Data Visualization', 4, 'Matplotlib, Plotly, Seaborn, Folium'],
+                                     ['Statistics', 4, 'Inferential and Descriptive'],
                                ]
              ],
 
             ["Software Engineering", [['Front End', 4, 'HTML, CSS'],  # language, level, years
                                      ['Frameworks', 2, 'Flask, Dash, Jinja'],
-                                     ['Database', 2, 'SQLite'],
+                                     ['Database', 3, 'SQLite'],
                                      ['Deployment', 2, 'Heroku, Python Anywhere, Twilio'],
                                ]
              ],
