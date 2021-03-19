753802259ddb376920a8339892908bc8bb9b6f94
Aaron Lee <amlee73@gmail.com>
2021-03-19 10:32:38 -0500

    Testing css changes

--- app.py
+++ app.py
@@ -1,37 +1,55 @@
-from flask import Flask, render_template
+from flask import Blueprint, Flask, g, jsonify, render_template, request, Response, redirect, url_for, send_from_directory
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+import helper
+import os
 
-# Configure application
 app = Flask(__name__)
+
 
 @app.route('/index', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
-    greeting = 'Welcome to My Data Science Portfolio Website'
-
-    return render_template('/index.html',
-                            title = "Data Science Portfolio",
-                            id = "index",
-                            greeting=greeting)
+    # main page
+    return render_template('index.html',
+                                title_text=helper.messages['index'],
+                                title=helper.titles['index'],
+                                id="index")
 
 
-@app.route('/projects', methods=['POST', 'GET'])
-@app.route('/', methods=['POST', 'GET'])
-def projects():
-    greeting = 'Welcome to My Data Science Projects Page'
-    return render_template('/projects.html',
-                            greeting=greeting)
+@app.route('/portfolio', methods=['POST', 'GET'])
+def portfolio():
+    # get the title content for the portfolio page
+    title_text = helper.titles['portfolio']
+
+    return render_template('/portfolio.html',
+                            title_text=title_text,
+                            title="PROJECT PORTFOLIO",
+                            id="portfolio",
+                            projects=helper.projects,)
 
 
 @app.route('/about', methods=['POST', 'GET'])
-@app.route('/', methods=['POST', 'GET'])
 def about():
-    greeting = 'Welcome to My Data Science About Me Page'
+    # default language if user enters about without language preference in url
+    title_text = helper.about_title_text
+
+    skills = helper.skills
+
     return render_template('/about.html',
-                            greeting=greeting)
+                            title_text=title_text,
+                            skills=skills,
+                            title="ABOUT ME",
+                            id="about")
 
 
-@app.route('/blog', methods=['POST', 'GET'])
-@app.route('/', methods=['POST', 'GET'])
-def blog():
-    return render_template('/blog.html')
+
+@app.route("/robots.txt")
+def robots():
+    '''
+    Add robots.txt file to avoid google indexing
+    '''
+    return send_from_directory(app.static_folder, request.path[1:])
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ helper.py
@@ -0,0 +1,46 @@
+
+
+# put all my changing info here
+# gets imported into app.py
+
+titles = {'portfolio': 'Portfolio Title from helper',
+          'about': 'About Title from helper',
+          'index': 'Index Title from helper',
+          }
+
+messages = {'portfolio': 'Portfolio message from helper',
+          'about': 'About message from helper',
+          'index': 'Index message from helper',
+          }
+
+
+
+
+about_title_text = "About title text"
+skills = [["Programming Languages", [['Python', 4, '7+ years'],  # language, level, years
+                                     ['C++', 2, '2 years'],
+                                     ['JavaScript', 2, '2 years'],
+                                     ['Java', 1, '2 years'],
+                                     ['Arduino', 3, '7+ years'],
+                                     ] ],
+            ["Data Analysis", [['Data Wrangling', 4, 'Pandas, NumPy, '],  # language, level, years
+                                     ['Data Visualization', 3, 'Matplotlib, Plotly, Seaborn, Folium'],
+                                     ['Statistics', 3, 'Inferential and Descriptive'],
+                                     ['Java', 2, '2 years']
+                               ]
+             ],
+
+            ["Software Engineering", [['Front End', 4, 'HTML, CSS'],  # language, level, years
+                                     ['Frameworks', 2, 'Flask, Dash, Jinja'],
+                                     ['Database', 2, 'SQLite'],
+                                     ['Deployment', 2, 'Heroku, Python Anywhere, Twilio'],
+                               ]
+             ],
+
+            ["Machine Learning", [['Supervised', 4, 'Classification, Regression'],  # language, level, years
+                                     ['Unsupervised', 3, 'CNN, K-means'],
+                                     ['Deep Learning', 1, 'TensorFlow'],
+                               ]
+             ],
+
+            ]
