860f62a151763551004fb45e66cbef763bbaa753
Aaron Lee <amlee73@gmail.com>
2021-05-27 17:35:37 -0500

    added website to my apps page

--- helper.py
+++ helper.py
@@ -22,9 +22,6 @@ messages = {'portfolio': ['Featured below are some of my recent Data Science pro
                     "I write for four different publications on Medium, including, '{}', '{}', and '{}'".format('The Startup', 'Towards Data Science', 'Level Up Coding', 'Python in Plain English', 'CodeX', 'Data Driven Investor', 'Geek Culture')],
            'apps': ['This website was made using Python and Flask.',
                       'I also chose to make companion webapps for selected data projects.  As a CS teacher, I taught Flask, '
-                      'Twilio, Heroku, and Kivy for deployment of small apps. I also mentored students in development of '
-                      'web apps for robotics using Android Studio.  Though not a developer, I have a good feel for the structure '
-                      'and flow of an app, and hope to continue learning app development going forward.'
                       ]
           }
 
